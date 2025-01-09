aa57d91bbc2b0494d2e4e5e91bc98dba9c488dfe
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-09 19:47:21 +0530

    Some gotchas

--- DirectInitialization.cc
+++ DirectInitialization.cc
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
   int a (4);
-  std::cout << a << endl;
+  // int b = (int) 5.6;  // compiles
+  // int b = 5.6;  // compile error
+  std::cout << a << endl;// << b << endl;
   return 0;
 }
--- ListInitialization.cc
+++ ListInitialization.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+  // int a = (int){ 5.5 }; // compile error
   int a = { 5 };
   std::cout << a << endl;
   return 0;
