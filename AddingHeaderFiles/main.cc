7a1d0daa6fe169476dd4ce63291635077bf2842d
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-15 20:14:09 +0530

    Update to comments and .gitignore

--- AddingHeaderFiles/main.cc
+++ AddingHeaderFiles/main.cc
@@ -1,8 +1,8 @@
 #include "add.h"
 #include <iostream>
 
-// g++ -o AddingHeaderFiles/main -I./AddingHeaderFiles AddingHeaderFiles/main.cc AddingHeaderFiles/add.cc
-// ./AddingHeaderFiles/main
+// g++ -o main -I./AddingHeaderFiles AddingHeaderFiles/main.cc AddingHeaderFiles/add.cc
+// ./main
 int main()
 {
   std::cout << "The sum of 3 and 4 is " << sum(3, 4) << std::endl;
