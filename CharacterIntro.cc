8c5e8c9206ae913cb19ae3c8ef1e7d43672ee93d
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-20 18:34:16 +0530

    Intro to characters

---
+++ CharacterIntro.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  char c;
+  std::cout << "Please enter a character \n";
+  std::cin >> c;
+  std::cout << "You entered: " << c << "\n";
+  std::cout << "Please enter the next character \n";
+  std::cin >> c;
+  std::cout << "You entered: " << c << "\n";
+}
