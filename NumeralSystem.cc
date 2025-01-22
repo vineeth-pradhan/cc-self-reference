030d97e4897648eddfba4485ee3b07d147960b99
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-22 21:49:44 +0530

    Documentation and bitset representation of int

--- NumeralSystem.cc
+++ NumeralSystem.cc
@@ -3,12 +3,17 @@
 
 int main()
 {
-  //        (0011'1010) 2
-  //        (0+0+32+16+8+0+2+0=58) 10
-  //        (00'111'010 = 72) 8
-  int x = 0x3A;
+  //        (0011'1010) Binary
+  //        (0+0+32+16+8+0+2+0=58) Decimal
+  //        (00'111'010 = 72) Octal
+  int x = 0x3A; // Hexa
+  //
+  //        (00010101) Binary
+  //        ()
+  int b = 21;   // 10
 
   std::cout << x << "\n";
   std::cout << std::hex << x << "\n";
   std::cout << std::oct << x << "\n";
+  std::cout << std::bitset<8>(b) << "\n";
 }
