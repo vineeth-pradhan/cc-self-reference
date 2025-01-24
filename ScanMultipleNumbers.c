991a4789a62b77e4ff3f28e179a449c58d71df27
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-24 12:58:10 +0530

    First c program and related gitignore change

---
+++ ScanMultipleNumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int number1;
+  printf("Enter a number\n");
+  scanf("%d", &number1);
+  printf("You entered %d\n", number1);
+  int number2;
+  printf("Enter a second number\n");
+  scanf("%d", &number2);
+  printf("You entered %d\n", number2);
+  return 0;
+}
