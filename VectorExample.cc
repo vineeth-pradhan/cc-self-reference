2c6971fd62fd11a01ab8652345473e96587fcbfd
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-24 19:47:07 +0530

    Vector init size

--- VectorExample.cc
+++ VectorExample.cc
@@ -18,4 +18,16 @@ int main()
     std::cout << name[i];
   }
   std::cout << "\n";
+
+  std::vector<int> list { 10 };
+  std::cout << "The size of the list is " << list.size() << "\n";
+
+  std::vector<int> list2(10);
+  std::cout << "This size of the list is " << list2.size() << "\n";
+
+  const int list2Len = list2.size();
+  for(int i = 0; i < list2Len - 1; i++) {
+    std::cout << list2[i] << ", ";
+  }
+  std::cout << list2[list2Len - 1] << "\n";
 }
