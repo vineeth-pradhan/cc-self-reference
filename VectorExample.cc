c4789060b1fa4651d7d1d19da029c20575a77e6a
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-24 18:22:20 +0530

    Vector with strings

--- VectorExample.cc
+++ VectorExample.cc
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <vector>
 
+// g++ -o main -std=c++23 VectorExample.cc
+// ./main
 int main()
 {
-  std::vector<int> digits { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  std::vector<int> digits { 1, 6, 0, 7, 1, 9, 8, 7 };
   const int digitsLen = digits.size();
   for(int i = 0; i < digitsLen - 1; i++){
     std::cout << digits[i] << ", ";
   }
   std::cout << digits[digitsLen - 1] << "\n";
 
-  std::vector name { 'v', 'i', 'n', 'e', 'e', 't', 'h'};
+  std::vector name { 'V', 'i', 'n', 'e', 'e', 't', 'h' };
   const int nameLen = name.size();
   for(int i = 0; i < nameLen; i++) {
-    std::cout << nameLen[i];
+    std::cout << name[i];
   }
   std::cout << "\n";
 }
