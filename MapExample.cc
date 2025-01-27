c3b24997dfa14f929084fafeef10b368b49ac486
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-27 14:27:48 +0530

    Print map with indentation

--- MapExample.cc
+++ MapExample.cc
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <map>
 
+void print_map(std::map<std::string_view, std::string_view> &m)
+{
+  std::cout << "{\n";
+  for(const auto& [key, value]: m) {
+    // 2 space indent
+    std::cout << "  ";
+    std::cout << key << ": " << value << "\n";
+  }
+  std::cout << "}\n";
+}
+
 // g++ -o main -std=c++23 MapExample.cc
 int main()
 {
-  std::map<std::string_view, std::string_view> people {{ "name", "Vineeth" }, { "age", "37" }};
-  std::cout << people["name"] << "\n";
-  std::cout << people["age"] << "\n";
+  using namespace std::string_literals;
+
+  std::map<std::string_view, std::string_view> person {{ "name", "Vineeth" }, { "age", "37" }};
+  // std::cout << person["name"] << "\n";
+  // std::cout << person["age"] << "\n";
+  // std::cout << person["age"].size() << "\n";
+  person.insert({"ph"s, "9008909779"});
+  // std::cout << person["ph"] << "\n";
+  print_map(person);
 }
