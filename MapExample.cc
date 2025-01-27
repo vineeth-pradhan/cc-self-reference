13c2eb80d132c8ca3fae4c28393ad562fb49686d
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-27 19:04:39 +0530

    Map's utility methods

--- MapExample.cc
+++ MapExample.cc
@@ -12,16 +12,46 @@ void print_map(std::map<std::string_view, std::string_view> &m)
   std::cout << "}\n";
 }
 
+
+void print_map2(std::map<std::string_view, std::string_view> &m)
+{
+  std::cout << "Alternate way of printing map" << "\n";
+  std::cout << "{" << "\n";
+  // Use cbegin/cend for readonly. For modify use begin/end
+  for(auto it = m.cbegin(); it != m.cend(); it++) {
+    std::cout << "  ";
+    // If something should be modified, change the cbegin to begin
+    // and cend to end above
+    //
+    // if(it -> first == "name")
+    //   it -> second = "Pradhan, Vineeth";
+
+    std::cout << it -> first << ": " << it -> second << "\n";
+  }
+  std::cout <<"}" << "\n";
+}
+
 // g++ -o main -std=c++23 MapExample.cc
+// ./main
 int main()
 {
-  using namespace std::string_literals;
-
   std::map<std::string_view, std::string_view> person {{ "name", "Vineeth" }, { "age", "37" }};
   // std::cout << person["name"] << "\n";
   // std::cout << person["age"] << "\n";
   // std::cout << person["age"].size() << "\n";
-  person.insert({"ph"s, "9008909779"});
+  person.insert({"ph", "9008909779"});
+  person.insert({"address", "6163, Cedar block 6, SFV, BSK 6, BLRU - 109"});
   // std::cout << person["ph"] << "\n";
   print_map(person);
+  // Prints out the occurance of name in the map
+  std::cout << "Occurance of 'name': "<< person.count("name") << "\n";
+
+  // Prints out the total size of the map
+  std::cout << "Total size: " << person.size() << "\n";
+
+  // Prints the total size of that the map can accommodate
+  std::cout << "Max size: " << std::scientific << static_cast<double>(person.max_size()) << "\n";
+
+  // Prints the map an alternate way
+  print_map2(person);
 }
