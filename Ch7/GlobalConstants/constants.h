6cbf8c4dbc441fc549221d885ca1767ba1299413
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-03-11 19:53:31 +0530

    Setting up GlobalConstants experiment

---
+++ Ch7/GlobalConstants/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+namespace constants
+{
+  constexpr double pi { 3.14159 };
+  constexpr double gravity { 9.8 };
+  constexpr double avogadro { 6.0221413e23 };
+}
+#endif
